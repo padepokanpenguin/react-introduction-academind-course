83db17fd35a4062dfc0b5e49ac892b34f4054c32
Aria Nur Jamal <jamal.psw@gmail.com>
2022-02-13 14:44:58 +0700

    using one state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,35 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () =>  {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [amount, setAmount] = useState('');
-  const [date, setDate] = useState('');
+  //const [enteredTitle, setEnteredTitle] = useState('');
+  //const [amount, setAmount] = useState('');
+  //const [date, setDate] = useState('');
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: ''
+  }) 
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   }
 
   const amountChangeHandler = (event) => {
-    setAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   }
 
   const dateChangeHandler = (event) => {
-     setDate(event.target.value);
+     setUserInput({
+       ...UserInput,
+       enteredDate: '',
+     })
   }
 
   return <form>
